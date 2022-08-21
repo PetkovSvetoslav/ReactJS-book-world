a7c45f84a728dc67e7690e2f62866653f77de01a
Svetoslav Krasimirov Petkov <98747798+PetkovSvetoslav@users.noreply.github.com>
2022-08-21 10:02:19 +0300

    Update and rename src/components/Footer.js to src/components/Footer/Footer.js

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export   const Footer = () => {
-    return (
-        <footer className="footer">
-          <p>Copyright © designed by Svetoslav K. Petkov</p>
-        </footer>
-    );
-}
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer id="site-footer">
+            <p>@MyBookWorld</p>
+        </footer>
+    );
+};
+
+export default Footer;
