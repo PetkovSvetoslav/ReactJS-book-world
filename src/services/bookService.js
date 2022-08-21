5fe1311afd3d1704aebd84f61bf4acce8003afe3
Svetoslav Krasimirov Petkov <98747798+PetkovSvetoslav@users.noreply.github.com>
2022-08-21 09:50:56 +0300

    Update bookService.js

--- src/services/bookService.js
+++ src/services/bookService.js
@@ -1,13 +1,40 @@
-const baseUrl = 'http://localhost:3030/data';
+const baseUrl = 'http://localhost:3030/data'
 
-export function getAll() {
-    return fetch(`${baseUrl}/books?sortBy=_createdOn%20desc`)
-        .then(res => res.json())
+async function getAll() {
+    const res = await fetch(`${baseUrl}/books`);
+    return res.json();
 }
 
-export const getOne = (id) => fetch(`${baseUrl}/books/${id}`).then(res => res.json());
+async function getById(id) {
+    const res = await fetch(`${baseUrl}/books/${id}`);
+    return res.json();
+}
 
-export const getLatest = () => {
-    return fetch(`${baseUrl}/books?sortBy=_createdOn%20desc&distinct=category`)
-        .then(res => res.json())
+async function create(data, token) {
+    await fetch(`${baseUrl}/books`, {
+        method : 'post',
+        headers : {
+            'content-type' : 'application/json',
+            'X-Authorization' : token
+        },
+        body : JSON.stringify({...data, likes: 0})
+    });
+}
+
+async function deletePet(id, token) {
+    await fetch(`${baseUrl}/books/${id}`, {
+        method: 'delete',
+        headers: {
+            'X-Authorization' : token
+        }
+    });
+
+
+}
+
+export {
+    getAll,
+    create,
+    getById,
+    deletePet
 }
